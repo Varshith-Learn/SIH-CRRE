7016100e757a3b6d60101dc8f155c5bedd9a9601
DEEVEN SERU <github@deeven.me>
2026-09-02 11:56:37 +0530

    Configure GitHub Pages deployment via Actions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isExport = process.env.IS_STATIC_EXPORT === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: isExport ? 'export' : undefined,
   images: {
+    unoptimized: isExport ? true : undefined,
     remotePatterns: [
       {
         protocol: 'https',
@@ -9,33 +14,35 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
-          },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/problems',
-        destination: 'https://sih.gov.in/sih2026PS',
-        permanent: false,
-      },
-      {
-        source: '/problem-statements',
-        destination: 'https://sih.gov.in/sih2026PS',
-        permanent: false,
-      },
-    ];
-  },
+  ...(isExport ? {} : {
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+            },
+          ],
+        },
+      ];
+    },
+    async redirects() {
+      return [
+        {
+          source: '/problems',
+          destination: 'https://sih.gov.in/sih2026PS',
+          permanent: false,
+        },
+        {
+          source: '/problem-statements',
+          destination: 'https://sih.gov.in/sih2026PS',
+          permanent: false,
+        },
+      ];
+    },
+  })
 };
 
 module.exports = nextConfig;
